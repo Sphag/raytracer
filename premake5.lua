20c57ccebc21853302c733120ae87b4705b60196
Max Bukhalov <buchalovmaxim@gmail.com>
2020-04-20 12:52:41 +0300

    removed libcds from premake

--- premake5.lua
+++ premake5.lua
@@ -52,11 +52,6 @@ workspace "raytracer"
 		"raytracer/vendor/tinyobjloader/tiny_obj_loader.cc",
 	}
 
-	links
-	{
-		"cds"
-	}
-
 	defines
 	{
 		"_CRT_SECURE_NO_WARNINGS",
@@ -68,7 +63,6 @@ workspace "raytracer"
 		"raytracer/%{prj.name}/include",
 		"raytracer/vendor/glm",
 		"raytracer/vendor/stb_image/",
-		"raytracer/vendor/libcds/",
 		"raytracer/vendor/tinyobjloader/",
 	}
 
@@ -76,49 +70,3 @@ workspace "raytracer"
 	filter {"files:raytracer/vendor/stb_image/**.cpp"}
 		flags {"NoPCH"}
 	filter { }
-
-
-	project "cds"
-	location "raytracer/vendor/libcds/"
-	kind "SharedLib"
-	language "C++"
-	cppdialect "C++17"
-
-	filter { "configurations:Debug" }
-		postbuildcommands { "copy ..\\..\\..\\build\\bin\\cds\\Debug\\cds.dll ..\\..\\..\\build\\bin\\raytracer\\Debug\\" }
-	filter { }
-	filter { "configurations:Release" }
-		postbuildcommands { "copy ..\\..\\..\\build\\bin\\cds\\Release\\cds.dll ..\\..\\..\\build\\bin\\raytracer\\Release\\" }
-	filter { }
-	filter { "configurations:Debug" }
-		defines
-		{
-			"_ENABLE_ATOMIC_ALIGNMENT_FIX",
-			"WIN32",
-			"_DEBUG",
-			"_CONSOLE",
-			"_WIN32_WINNT=0x0501",
-			"CDS_BUILD_LIB"
-		}
-	filter { }
-	filter { "configurations:Release" }
-		defines
-		{
-			"_ENABLE_ATOMIC_ALIGNMENT_FIX",
-			"WIN32",
-			"NDEBUG",
-			"_CONSOLE",
-			"_WIN32_WINNT=0x0501",
-			"CDS_BUILD_LIB",
-		}
-	filter { }
-	includedirs
-	{
-		"raytracer/vendor/libcds/",
-	}
-
-	files
-	{
-		"raytracer/vendor/libcds/cds/**",
-		"raytracer/vendor/libcds/src/**.cpp",
-	}
